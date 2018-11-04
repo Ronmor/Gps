081ae36095d5140daa4336743a1c0ba490e9ac9f
ben-moshe <benmo@g.ariel.ac.il>
2018-11-04 15:35:04 +0200

    simple text file read & write example

--- OOP_Ex1/src/Text_101.java
+++ OOP_Ex1/src/Text_101.java
@@ -4,7 +4,6 @@ public class Text_101 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i=3;
-		kdjsfHlsd;fk
 		System.out.println("Hello World Testing git!");
 	}
 
