4e7d575b69625b206c31ceca9704863b62cda99d
ben-moshe <benmo@g.ariel.ac.il>
2018-11-04 12:59:33 +0200

    This is a simple example of a trivial commit

--- OOP_Ex1/src/Text_101.java
+++ OOP_Ex1/src/Text_101.java
@@ -3,7 +3,7 @@ public class Text_101 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		System.out.println("Hello World Testing git!");
 	}
 
 }
