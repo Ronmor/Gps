147a49edff4f68623fbd03733da98641452f14c9
ben-moshe <benmo@g.ariel.ac.il>
2018-11-17 22:19:17 +0200

    Simple Threads example of Sorting with two threads in parallel vs serial sorting.

--- OOP_EX2-Ex4/src/Threads/TestThreads.java
+++ OOP_EX2-Ex4/src/Threads/TestThreads.java
@@ -2,8 +2,8 @@ package Threads;
 
 public class TestThreads {
 	public static void main(String[] a) {
-		MyThread t0 = new MyThread("aaaaaaaaaaaaaaaaaaaaaa");
-		MyThread t1 = new MyThread("bbbbbbbbbbbbbbbbbbbbbb");
+		MyThread t0 = new MyThread("a");
+		MyThread t1 = new MyThread("b");
 	//	t0.run();
 	//	t1.run();
 		t0.start();
